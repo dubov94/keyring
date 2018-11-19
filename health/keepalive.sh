38bb9dc36c9667fb2cd6b72a4f764de357f9f8ee
Pavel Dubov <dubov94@gmail.com>
2018-11-20 00:26:19 +0100

    Mute curl progress output

--- health/keepalive.sh
+++ health/keepalive.sh
@@ -2,7 +2,7 @@
 while true
 do
   date
-  curl --location pwd.floreina.me/api/authentication/get-salt/anonymous
+  curl --location --silent --show-errors pwd.floreina.me/api/authentication/get-salt/anonymous
   echo
   sleep 60
 done
