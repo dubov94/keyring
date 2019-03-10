7149225a9b694c0b38c03441ce3703f9123d7a4c
Pavel Dubov <dubov94@gmail.com>
2019-03-10 21:05:02 +0000

    Make navigation drawer a separate component

---
+++ client/source/components/toolbar-with-menu/Toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-toolbar app clipped-left prominent color="primary" dark>
+    <v-toolbar-side-icon @click="toggle"></v-toolbar-side-icon>
+    <slot>
+      <v-toolbar-title>
+        Key Ring
+      </v-toolbar-title>
+    </slot>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    props: ['value'],
+    methods: {
+      toggle () {
+        this.$emit('input', !this.value)
+      }
+    }
+  }
+</script>
