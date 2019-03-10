be30fef746d65b41c3fbaf9f146b3345df6beca8
Pavel Dubov <dubov94@gmail.com>
2019-03-10 21:48:26 +0000

    Add a connection indicator

--- client/source/components/toolbar-with-menu/SideMenu.vue
+++ client/source/components/toolbar-with-menu/SideMenu.vue
@@ -2,7 +2,7 @@
   <v-navigation-drawer app temporary clipped floating
     :value="value" @input="input">
     <v-list>
-      <v-list-tile to="/dashboard">
+      <v-list-tile @click="$router.push('/dashboard')">
         <v-list-tile-action>
           <v-icon>dashboard</v-icon>
         </v-list-tile-action>
@@ -10,7 +10,7 @@
           Dashboard
         </v-list-tile-content>
       </v-list-tile>
-      <v-list-tile to="/settings">
+      <v-list-tile @click="$router.push('/settings')">
         <v-list-tile-action>
           <v-icon>fa-cog</v-icon>
         </v-list-tile-action>
@@ -18,12 +18,26 @@
           Settings
         </v-list-tile-content>
       </v-list-tile>
+      <v-divider></v-divider>
       <v-list-tile @click="logOut">
         <v-list-tile-action>
           <v-icon>fa-sign-out-alt</v-icon>
         </v-list-tile-action>
         <v-list-tile-content>
           Log out
+        </v-list-tile-content>
+      </v-list-tile>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon color="success">wifi</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Online
+          </v-list-tile-title>
+          <v-list-tile-sub-title>
+            Click to disconnect
+          </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
