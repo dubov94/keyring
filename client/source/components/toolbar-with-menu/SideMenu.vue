618b2269a3a327537ef74c8df871d622d454a70c
Pavel Dubov <dubov94@gmail.com>
2019-06-01 14:30:30 +0100

    Move the connection indicator to the top

--- client/source/components/toolbar-with-menu/SideMenu.vue
+++ client/source/components/toolbar-with-menu/SideMenu.vue
@@ -2,6 +2,19 @@
   <v-navigation-drawer app temporary clipped floating
     :value="value" @input="input">
     <v-list>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon color="success">wifi</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Online
+          </v-list-tile-title>
+          <v-list-tile-sub-title>
+            Click to disconnect
+          </v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
       <v-list-tile @click="$router.push('/dashboard')">
         <v-list-tile-action>
           <v-icon>dashboard</v-icon>
@@ -25,19 +38,6 @@
         </v-list-tile-action>
         <v-list-tile-content>
           Log out
-        </v-list-tile-content>
-      </v-list-tile>
-      <v-list-tile>
-        <v-list-tile-action>
-          <v-icon color="success">wifi</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>
-            Online
-          </v-list-tile-title>
-          <v-list-tile-sub-title>
-            Click to disconnect
-          </v-list-tile-sub-title>
         </v-list-tile-content>
       </v-list-tile>
     </v-list>
