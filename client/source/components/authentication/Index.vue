74aff279f1d8704a5b8e467412eabf9f87de41bf
Pavel Dubov <dubov94@gmail.com>
2019-10-25 01:46:54 +0100

    Remove the note about browser managers

--- client/source/components/authentication/Index.vue
+++ client/source/components/authentication/Index.vue
@@ -4,9 +4,6 @@
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
           <v-flex xs12 sm6 md4 lg3 xl2>
-            <p class="text-xs-center text--secondary">
-              Do not save your password in the browser!
-            </p>
             <router-view></router-view>
           </v-flex>
         </v-layout>
