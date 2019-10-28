0766602c3c5b366c355282935a81d23acb2ff5e9
Pavel Dubov <dubov94@gmail.com>
2019-10-28 08:14:49 +0000

    Disable remote action buttons in Editor when !isOnline

--- client/source/components/Editor.vue
+++ client/source/components/Editor.vue
@@ -97,7 +97,7 @@
         <v-progress-circular indeterminate color="green"
           v-if="requestInProgress"></v-progress-circular>
         <v-spacer></v-spacer>
-        <v-btn flat color="error" :disabled="requestInProgress"
+        <v-btn flat color="error" :disabled="!isOnline || requestInProgress"
           v-if="identifier !== null" @click="maybeRemove">
           Remove
         </v-btn>
@@ -105,7 +105,7 @@
           @click="maybeDiscard">
           Cancel
         </v-btn>
-        <v-btn flat color="primary" :disabled="requestInProgress"
+        <v-btn flat color="primary" :disabled="!isOnline || requestInProgress"
           @click="save">
           Save
         </v-btn>
@@ -121,7 +121,7 @@
 <script>
   import Draggable from 'vuedraggable'
   import YesNoDialog from './YesNoDialog'
-  import {mapActions, mapMutations} from 'vuex'
+  import {mapActions, mapGetters, mapMutations} from 'vuex'
   import {XL_MINIMAL_WIDTH} from './constants'
   import {
     areArraysEqual,
@@ -166,6 +166,9 @@
       window.removeEventListener('beforeunload', this.onBeforeUnload)
     },
     computed: {
+      ...mapGetters({
+        isOnline: 'isOnline'
+      }),
       isVisible () {
         return this.$store.state.interface.editor.show
       }
