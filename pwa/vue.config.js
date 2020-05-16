e8bf98ba4ace9274abfc6ba32d830b9d04bfd0ca
Pavel Dubov <dubov94@gmail.com>
2020-05-16 11:40:13 +0100

    Add workerize-loader to vue.config.js

--- pwa/vue.config.js
+++ pwa/vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  indexPath: 'entry.html'
+  indexPath: 'entry.html',
+  '^/api': {
+    target: 'http://localhost:5001',
+    changeOrigin: true
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('workerize')
+      .test(/\.worker\.js$/)
+      .use('workerize-loader')
+        .loader('workerize-loader')
+        .options({
+          inline: true
+        })
+  }
 }
