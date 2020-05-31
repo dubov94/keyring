952519211e6214fcaf47b4c3f1c2e5c3a6507620
Pavel Dubov <dubov94@gmail.com>
2020-05-31 06:32:16 +0100

    Add icons and manifests to pwa/

--- pwa/vue.config.js
+++ pwa/vue.config.js
@@ -33,5 +33,19 @@ module.exports = {
         }
         return args
       })
+  },
+  pwa: {
+    name: 'Key Ring',
+    themeColor: '#1976d2',
+    msTileColor: '#2d89ef',
+    assetsVersion: 'A0mpzYva75',
+    manifestPath: 'site.webmanifest',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/mstile-144x144.png'
+    }
   }
 }
