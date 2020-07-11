3112bb6e10781d03a8a91a6dda32831d2cb8cbb4
Pavel Dubov <dubov94@gmail.com>
2020-07-12 00:13:56 +0100

    Rewrite cache expiration without CacheExpiration

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -1,7 +1,3 @@
-workbox.core.setCacheNameDetails({
-  prefix: 'keyring.pwa'
-});
-
 const getRequestUrl = (value) => {
   if (typeof value === 'string') {
     return value;
@@ -12,48 +8,59 @@ const getRequestUrl = (value) => {
   }
 };
 
-const patchCacheWithExpiration = (cacheExpiration) => {
+const getResponseTimestamp = (response) => {
+  if (!response) {
+    return null;
+  }
+  const dateHeader = response.headers.get('date');
+  if (!dateHeader) {
+    return null;
+  }
+  const timestamp = new Date(dateHeader).getTime();
+  if (isNaN(timestamp)) {
+    return null;
+  }
+  return timestamp;
+};
+
+const patchCacheWithExpiration = (
+    targetName, precacheManifest, shellCacheKey, maxAgeSeconds) => {
   const oCachesOpen = caches.open.bind(caches);
   caches.open = async (cacheName) => {
-    if (cacheName === cacheExpiration._cacheName) {
-      const cache = await oCachesOpen(cacheName);
+    const cache = await oCachesOpen(cacheName);
+    if (cacheName === targetName) {
       const oCacheMatch = cache.match.bind(cache);
       cache.match = async (request, options) => {
-        // Mimic `workbox.expiration.Plugin.cachedResponseWillBeUsed`.
-        await cacheExpiration.expireEntries();
-        await cacheExpiration.updateTimestamp(getRequestUrl(request));
-
-        return await oCacheMatch(request, options);
+        if (getRequestUrl(request) === shellCacheKey) {
+          const timestamp = getResponseTimestamp(await oCacheMatch(shellCacheKey));
+          if (timestamp === null || timestamp < Date.now() - maxAgeSeconds * 1000) {
+            const ok = await Promise.all(precacheManifest.map(({ url }) =>
+                cache.delete(workbox.precaching.getCacheKeyForURL(url))));
+            if (!ok) {
+              throw new Error('Unable to expire entries');
+            }
+          }
+        }
+        return oCacheMatch(request, options);
       };
-      return cache;
-    } else {
-      return await oCachesOpen(cacheName);
     }
+    return cache;
   };
 };
 
-const cacheExpiration = new workbox.expiration.CacheExpiration(
+workbox.core.setCacheNameDetails({ prefix: 'keyring.pwa' });
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+const indexCacheKey = workbox.precaching.getCacheKeyForURL('/index.html');
+workbox.routing.registerNavigationRoute(indexCacheKey);
+patchCacheWithExpiration(
   workbox.core.cacheNames.precache,
-  { maxAgeSeconds: 2 * 28 * 24 * 60 * 60 }
+  self.__precacheManifest,
+  indexCacheKey,
+  2 * 28 * 24 * 60 * 60
 );
-workbox.precaching.addPlugins([
-  {
-    // Mimic `workbox.expiration.Plugin.cacheDidUpdate`.
-    async cacheDidUpdate ({ request }) {
-      await cacheExpiration.updateTimestamp(getRequestUrl(request));
-      await cacheExpiration.expireEntries();
-    }
-  }
-]);
-patchCacheWithExpiration(cacheExpiration);
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
-
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
-
-const indexCacheKey = workbox.precaching.getCacheKeyForURL('/index.html');
-workbox.routing.registerNavigationRoute(indexCacheKey);
