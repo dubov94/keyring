bd34727efc4c51bab491a6b349877cc0b1a158d9
Pavel Dubov <dubov94@gmail.com>
2020-08-15 03:19:38 +0100

    Migrate keys.js to TS

--- pwa/src/store/root/actions/index.ts
+++ pwa/src/store/root/actions/index.ts
@@ -2,7 +2,7 @@ import { ActionTree } from 'vuex'
 import { RootState } from '../state'
 import { AccountActions as Account } from './account'
 import Authentication from './authentication'
-import Keys from './keys'
+import { KeysActions as Keys } from './keys'
 import { MailActions as Mail } from './mail'
 
 export const RootActions: ActionTree<RootState, RootState> = Object.assign({}, Account, Authentication, Keys, Mail)
---
+++ pwa/src/store/root/actions/keys.ts
@@ -0,0 +1,82 @@
+import { ActionTree } from 'vuex'
+import { RootState, Key } from '@/store/root/state'
+import { container } from 'tsyringe'
+import { SodiumClient } from '@/sodium_client'
+import { createSessionHeader } from './utilities'
+import {
+  AdministrationApi,
+  ServiceCreateKeyResponse
+} from '@/api/definitions'
+import { Type as RootMutation } from '../mutations'
+import { ActionType as DepotAction } from '@/store/modules/depot'
+import { ActionType as ThreatsAction } from '@/store/modules/threats'
+
+export const KeysActions: ActionTree<RootState, RootState> = {
+  async acceptUserKeys (
+    { commit, dispatch, state },
+    { userKeys, updateDepot }: { userKeys: Array<Key>; updateDepot: boolean }
+  ) {
+    const { encryptionKey } = state
+    if (encryptionKey === null) {
+      throw new Error('`RootState.encryptionKey` is null')
+    }
+    commit(RootMutation.SET_USER_KEYS, await Promise.all(
+      userKeys.map(async ({ identifier, value, tags }) =>
+        Object.assign({ identifier }, await container.resolve(SodiumClient).decryptPassword(
+          encryptionKey, { value, tags }))
+      )
+    ))
+    if (updateDepot) {
+      await dispatch(`depot/${DepotAction.MAYBE_UPDATE_DEPOT}`, { userKeys: state.userKeys })
+    }
+    await dispatch(`threats/${ThreatsAction.MAYBE_ASSESS_USER_KEYS}`, state.userKeys)
+  },
+  async createUserKey (
+    { commit, dispatch, state },
+    { value, tags }: { value: string; tags: Array<string> }) {
+    if (state.sessionKey === null) {
+      throw new Error('`RootState.sessionKey` is null')
+    }
+    if (state.encryptionKey === null) {
+      throw new Error('`RootState.encryptionKey` is null')
+    }
+    const response: ServiceCreateKeyResponse =
+      await container.resolve(AdministrationApi).createKey({
+        password: await container.resolve(SodiumClient).encryptPassword(
+          state.encryptionKey, { value, tags })
+      }, { headers: createSessionHeader(state.sessionKey) })
+    commit(RootMutation.UNSHIFT_USER_KEY, { identifier: response.identifier, value, tags })
+    await dispatch(`depot/${DepotAction.MAYBE_UPDATE_DEPOT}`, { userKeys: state.userKeys })
+    await dispatch(`threats/${ThreatsAction.MAYBE_ASSESS_USER_KEYS}`, state.userKeys)
+  },
+  async updateUserKey (
+    { commit, dispatch, state }, { identifier, value, tags }: Key) {
+    if (state.sessionKey === null) {
+      throw new Error('`RootState.sessionKey` is null')
+    }
+    if (state.encryptionKey === null) {
+      throw new Error('`RootState.encryptionKey` is null')
+    }
+    await container.resolve(AdministrationApi).updateKey({
+      key: {
+        identifier,
+        password: await container.resolve(SodiumClient).encryptPassword(
+          state.encryptionKey, { value, tags })
+      }
+    }, { headers: createSessionHeader(state.sessionKey) })
+    commit(RootMutation.MODIFY_USER_KEY, { identifier, value, tags })
+    await dispatch(`depot/${DepotAction.MAYBE_UPDATE_DEPOT}`, { userKeys: state.userKeys })
+    await dispatch(`threats/${ThreatsAction.MAYBE_ASSESS_USER_KEYS}`, state.userKeys)
+  },
+  async removeUserKey ({ commit, dispatch, state }, { identifier }: { identifier: string }) {
+    if (state.sessionKey === null) {
+      throw new Error('`RootState.sessionKey` is null')
+    }
+    await container.resolve(AdministrationApi).deleteKey({ identifier }, {
+      headers: createSessionHeader(state.sessionKey)
+    })
+    commit(RootMutation.DELETE_USER_KEY, identifier)
+    await dispatch(`depot/${DepotAction.MAYBE_UPDATE_DEPOT}`, { userKeys: state.userKeys })
+    await dispatch(`threats/${ThreatsAction.MAYBE_ASSESS_USER_KEYS}`, state.userKeys)
+  }
+}
