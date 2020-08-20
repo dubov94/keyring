4f244685473594684de33bbf63f3dcc96d4fc0d9
Pavel Dubov <dubov94@gmail.com>
2020-08-20 07:28:07 +0100

    Remove obsolete applyGoOfflineOnRequestError

--- pwa/src/main.js
+++ pwa/src/main.js
@@ -11,9 +11,6 @@ import Vuelidate from 'vuelidate'
 import Vuetify from 'vuetify'
 import router from './router'
 import store from './store'
-import {
-  applyGoOfflineOnRequestError
-} from './aspects'
 
 Vue.use(VueI18n)
 Vue.use(VueRx)
@@ -32,18 +29,6 @@ Vue.directive('visible', (element, binding) => {
 })
 
 Vue.component('form-text-field', FormTextField)
-
-// Aspects are a very powerful short-term weapon, and as any powerful weapon
-// should be used with extreme care. While they do save quite a bit of time
-// on initial plumbing, they heavily obfuscate the logic and sometimes can
-// lead to undesired side-effects in places where their application was not
-// expected by the developer. We are actively trying to reduce their number,
-// but it's a perpetual battle -- in practice as soon as one is eliminated
-// it's a matter of time before another pops up.
-//
-// That said, given the migration to RxJS we may be able to get rid of all
-// of them soon!
-applyGoOfflineOnRequestError()
 
 sodium.ready.then(() =>
   new Vue({
