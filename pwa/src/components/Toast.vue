32c64b81943ec724b16ecb6e18c63ae8f741f37c
Pavel Dubov <dubov94@gmail.com>
2020-08-30 13:26:34 +0100

    Convert Toast.vue to TS

--- pwa/src/components/Toast.vue
+++ pwa/src/components/Toast.vue
@@ -1,36 +1,34 @@
 <template>
-  <v-snackbar v-model="isVisible" auto-height :timeout="timeout">
+  <v-snackbar :value="isVisible" @input="setVisibility" auto-height :timeout="timeout">
     {{ message }}
     <v-btn color="warning" flat @click="hideToast">Close</v-btn>
   </v-snackbar>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
 import { mapMutations, mapState } from 'vuex'
+import { InterfaceState } from '@/store/state'
 
-export default {
+export default Vue.extend({
   computed: {
-    isVisible: {
-      get () {
-        return this.$store.state.interface.toast.show
-      },
-      set (value) {
-        if (value === false) {
-          this.hideToast()
-        } else {
-          throw new Error('Snackbar requested a show.')
-        }
-      }
-    },
-    ...mapState('interface', {
-      message: state => state.toast.message,
-      timeout: state => state.toast.timeout
+    ...mapState<InterfaceState>('interface', {
+      message: (state) => state.toast.message,
+      timeout: (state) => state.toast.timeout,
+      isVisible: (state) => state.toast.show
     })
   },
   methods: {
+    setVisibility (value: boolean) {
+      if (value === false) {
+        this.hideToast()
+      } else {
+        throw new Error('Snackbar requested a show')
+      }
+    },
     ...mapMutations({
       hideToast: 'interface/hideToast'
     })
   }
-}
+})
 </script>
