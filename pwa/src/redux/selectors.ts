6257225c9c3517cd7f3e4da41e7ea3b0e5385904
Pavel Dubov <dubov94@gmail.com>
2020-12-06 10:17:18 +0000

    Move store subscription under state$ creation

--- pwa/src/redux/selectors.ts
+++ pwa/src/redux/selectors.ts
@@ -6,11 +6,11 @@ import { DeepReadonly } from 'ts-essentials'
 import { RootState } from './conjunction'
 import { store } from './store'
 
+export const state$ = new BehaviorSubject<RootState>(store.getState())
+
 store.subscribe(() => {
   state$.next(store.getState())
 })
-
-export const state$ = new BehaviorSubject<RootState>(store.getState())
 
 export const apply = <T>(selector: Selector<RootState, DeepReadonly<T>>): Observable<DeepReadonly<T>> => {
   return state$.pipe(map(selector), distinctUntilChanged(isEqual))
