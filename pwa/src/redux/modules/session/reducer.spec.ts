a25bc821bcf4bcb07dc30d02d38e4cb1dd35cb57
Pavel Dubov <dubov94@gmail.com>
2021-02-20 10:35:55 +0000

    Add tests for session/reducer.ts

---
+++ pwa/src/redux/modules/session/reducer.spec.ts
@@ -0,0 +1,67 @@
+import { success } from '@/redux/flow_signal'
+import { expect } from 'chai'
+import { authnViaApiSignal, registrationSignal } from '../authn/actions'
+import { logOut, usernameChangeSignal } from '../user/account/actions'
+import { rehydrateSession } from './actions'
+import reducer from './reducer'
+
+describe('registrationSignal', () => {
+  it('updates the username', () => {
+    const state = reducer(undefined, registrationSignal(success({
+      username: 'username',
+      parametrization: 'parametrization',
+      encryptionKey: 'encryptionKey',
+      sessionKey: 'sessionKey',
+      userKeys: []
+    })))
+
+    expect(state.username).to.equal('username')
+  })
+})
+
+describe('authnSignal', () => {
+  it('updates the username', () => {
+    const state = reducer(undefined, authnViaApiSignal(success({
+      username: 'username',
+      password: 'password',
+      parametrization: 'parametrization',
+      encryptionKey: 'encryptionKey',
+      sessionKey: 'sessionKey',
+      requiresMailVerification: false,
+      userKeys: []
+    })))
+
+    expect(state.username).to.equal('username')
+  })
+})
+
+describe('usernameChangeSignal', () => {
+  it('updates the username', () => {
+    const state = reducer(undefined, usernameChangeSignal(success({
+      before: 'usernameA',
+      update: 'usernameB'
+    })))
+
+    expect(state.username).to.equal('usernameB')
+  })
+})
+
+describe('rehydrateSession', () => {
+  it('updates the username', () => {
+    const state = reducer(undefined, rehydrateSession({
+      username: 'username'
+    }))
+
+    expect(state.username).to.equal('username')
+  })
+})
+
+describe('logOut', () => {
+  it('clears the username', () => {
+    const state = reducer({
+      username: 'username'
+    }, logOut())
+
+    expect(state.username).to.be.null
+  })
+})
