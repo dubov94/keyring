7219d472e05ddd036c152f9ebcadf8aed2d2a66e
Pavel Dubov <dubov94@gmail.com>
2021-02-22 00:36:56 +0000

    Replaces letters with numbers for key identifiers

--- pwa/src/redux/modules/user/keys/reducer.spec.ts
+++ pwa/src/redux/modules/user/keys/reducer.spec.ts
@@ -40,10 +40,10 @@ describe('semaphore', () => {
 
 describe('emplace', () => {
   it('sorts and sets the keys', () => {
-    const a = { identifier: 'A', value: 'b', tags: ['a', 'b'] }
-    const b = { identifier: 'B', value: 'a', tags: ['a', 'c'] }
-    const c = { identifier: 'C', value: 'a', tags: ['a', 'b', 'd'] }
-    const d = { identifier: 'D', value: 'a', tags: ['a', 'b'] }
+    const a = { identifier: '0', value: 'b', tags: ['a', 'b'] }
+    const b = { identifier: '1', value: 'a', tags: ['a', 'c'] }
+    const c = { identifier: '2', value: 'a', tags: ['a', 'b', 'd'] }
+    const d = { identifier: '3', value: 'a', tags: ['a', 'b'] }
     const state = reducer(undefined, emplace([a, b, c, d]))
 
     expect(state.userKeys).to.deep.equal([d, a, c, b])
@@ -52,8 +52,8 @@ describe('emplace', () => {
 
 describe('creationSignal', () => {
   it('prepends the new key', () => {
-    const a = { identifier: 'A', value: 'a', tags: [] }
-    const b = { identifier: 'A', value: 'b', tags: [] }
+    const a = { identifier: '0', value: 'a', tags: [] }
+    const b = { identifier: '0', value: 'b', tags: [] }
     const state = reduce(reducer, undefined, [
       emplace([a]),
       creationSignal(success(b))
@@ -66,19 +66,19 @@ describe('creationSignal', () => {
 describe('updationSignal', () => {
   it('updates an existing key', () => {
     const state = reduce(reducer, undefined, [
-      emplace([{ identifier: 'A', value: 'x', tags: [] }]),
-      updationSignal(success({ identifier: 'A', value: 'y', tags: ['t'] }))
+      emplace([{ identifier: '0', value: 'x', tags: [] }]),
+      updationSignal(success({ identifier: '0', value: 'y', tags: ['t'] }))
     ])
 
-    expect(state.userKeys).to.deep.equal([{ identifier: 'A', value: 'y', tags: ['t'] }])
+    expect(state.userKeys).to.deep.equal([{ identifier: '0', value: 'y', tags: ['t'] }])
   })
 })
 
 describe('deletionSignal', () => {
   it('deletes an existing key', () => {
     const state = reduce(reducer, undefined, [
-      emplace([{ identifier: 'A', value: '', tags: [] }]),
-      deletionSignal(success('A'))
+      emplace([{ identifier: '0', value: '', tags: [] }]),
+      deletionSignal(success('0'))
     ])
 
     expect(state.userKeys).to.be.empty
