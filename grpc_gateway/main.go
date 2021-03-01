b15f5b4f5bdda9b28594f6f97ce12de51112f85b
Pavel Dubov <dubov94@gmail.com>
2021-03-01 00:57:08 +0000

    Fix gateway_grpc_library importpath

--- grpc_gateway/main.go
+++ grpc_gateway/main.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 
-	gw "github.com/dubov94/keyring/grpc-gateway/service_gateway_library"
+	gw "github.com/dubov94/keyring/grpc_gateway/service_gateway_library"
 )
 
 var (
