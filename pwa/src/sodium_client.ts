ba4b6703139161255f49e7358d5845776ec2e291
Pavel Dubov <dubov94@gmail.com>
2021-03-05 05:07:21 +0000

    Refactor Argon2 parametrization serialization and parsing

--- pwa/src/sodium_client.ts
+++ pwa/src/sodium_client.ts
@@ -3,29 +3,72 @@ import { SODIUM_WORKER_INTERFACE_TOKEN, SodiumWorkerInterface } from './sodium_w
 import { Password } from '@/redux/entities'
 import { DeepReadonly } from 'ts-essentials'
 
-const ARGON2_DEFAULT_M = 64 * 1024 * 1024
-const ARGON2_DEFAULT_T = 1
-
 const AUTH_DIGEST_SIZE_IN_BYTES = 32
 const ENCRYPTION_KEY_SIZE_IN_BYTES = 32
-
-const PARAMETRIZATION_REGULAR_EXPRESSION = new RegExp(
-  '^\\$(argon2(?:i|d|id))\\$m=([1-9][0-9]*),t=([1-9][0-9]*),p=([1-9][0-9]*)\\$([A-Za-z0-9-_]{22})$'
-)
 
 export interface MasterKeyDerivatives {
   authDigest: string;
   encryptionKey: string;
 }
 
-// https://tools.ietf.org/html/draft-irtf-cfrg-argon2-12#section-4
-// https://github.com/golang/crypto/blob/5ea612d1eb830b38bc4e914e37f55311eb58adce/argon2/argon2.go
 interface Argon2Parametrization {
+  separator: '$';
   type: 'argon2i' | 'argon2d' | 'argon2id';
+  version: number;
   memoryInBytes: number;
   iterations: number;
   threads: number;
   salt: string;
+}
+
+// TODO: Re-enable version population once the grace period is over.
+const serializeArgon2Parametrization = (struct: Argon2Parametrization): string => {
+  // https://github.com/jedisct1/libsodium/blob/57d950a54e6f7743084092ba5d31b8fa0641eab2/src/libsodium/crypto_pwhash/argon2/argon2-encoding.c
+  return [
+    struct.separator,
+    struct.type,
+    struct.separator,
+    [
+      `m=${struct.memoryInBytes}`,
+      `t=${struct.iterations}`,
+      `p=${struct.threads}`
+    ].join(','),
+    struct.separator,
+    struct.salt
+  ].join('')
+}
+
+const parseArgon2Parametrization = (parametrization: string): Argon2Parametrization => {
+  const SEPARATOR = '\\$'
+  const TYPE = 'argon2(?:i|d|id)'
+  const N = '[1-9][0-9]*'
+  const BASE64 = '[A-Za-z0-9-_=]+'
+  const regExp = new RegExp([
+    '^',
+    SEPARATOR,
+    `(?<type>${TYPE})`,
+    SEPARATOR,
+    `(?:v=(?<version>${N}))?`,
+    SEPARATOR,
+    [
+      `m=(?<memoryInBytes>${N})`,
+      `t=(?<iterations>${N})`,
+      `p=(?<threads>${N})`
+    ].join(','),
+    SEPARATOR,
+    `(?<salt>${BASE64})`,
+    '$'
+  ].join(''))
+  const groups = regExp.exec(parametrization)!.groups!
+  return <Argon2Parametrization>{
+    separator: '$',
+    type: groups.type,
+    version: Number(groups.version || '19'),
+    memoryInBytes: Number(groups.memoryInBytes),
+    iterations: Number(groups.iterations),
+    threads: Number(groups.threads),
+    salt: groups.salt
+  }
 }
 
 @injectable()
@@ -34,25 +77,31 @@ export class SodiumClient {
 
   async generateNewParametrization (): Promise<string> {
     const salt = await this.sodiumWorkerInterface.toBase64(await this.sodiumWorkerInterface.generateSalt())
-    // https://github.com/jedisct1/libsodium/blob/57d950a54e6f7743084092ba5d31b8fa0641eab2/src/libsodium/crypto_pwhash/argon2/argon2-encoding.c
-    return `$argon2id$m=${ARGON2_DEFAULT_M},t=${ARGON2_DEFAULT_T},p=1$${salt}`
+    // https://tools.ietf.org/html/draft-irtf-cfrg-argon2-12#section-4
+    // https://github.com/golang/crypto/blob/5ea612d1eb830b38bc4e914e37f55311eb58adce/argon2/argon2.go
+    return serializeArgon2Parametrization({
+      separator: '$',
+      type: 'argon2id',
+      version: 19,
+      memoryInBytes: 64 * 1024 * 1024,
+      iterations: 1,
+      threads: 1,
+      salt
+    })
   }
 
   async _computeArgon2HashForDigestAndKey (parametrization: string, password: string): Promise<Uint8Array> {
-    const matches = PARAMETRIZATION_REGULAR_EXPRESSION.exec(parametrization)
-    if (matches === null) {
-      throw new Error(`Malformed parametrization: '${parametrization}'`)
+    const struct = parseArgon2Parametrization(parametrization)
+    if (struct.type !== 'argon2id' || struct.version !== 19 || struct.threads !== 1) {
+      throw new Error(`Unsupported Argon2 parametrization: ${JSON.stringify(struct)}`)
     }
-    const [, algorithm, mText, tText, pText, salt] = matches
-    const [m, t, p] = [mText, tText, pText].map(Number)
-    if (algorithm === 'argon2id' && p === 1) {
-      return this.sodiumWorkerInterface.computeHash(
-        t, m, await this.sodiumWorkerInterface.fromBase64(salt), password,
-        AUTH_DIGEST_SIZE_IN_BYTES + ENCRYPTION_KEY_SIZE_IN_BYTES)
-    } else {
-      throw new Error('Unsupported hashing parameters: ' +
-        `algorithm = '${algorithm}', lanes = '${p}'`)
-    }
+    return this.sodiumWorkerInterface.computeHash(
+      struct.iterations,
+      struct.memoryInBytes,
+      await this.sodiumWorkerInterface.fromBase64(struct.salt),
+      password,
+      AUTH_DIGEST_SIZE_IN_BYTES + ENCRYPTION_KEY_SIZE_IN_BYTES
+    )
   }
 
   async _extractAuthDigestAndEncryptionKey (hash: Uint8Array): Promise<MasterKeyDerivatives> {
