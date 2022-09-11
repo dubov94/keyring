addb1967b73ba63ffe92ac2a528e2cee6963817e
Pavel Dubov <dubov94@gmail.com>
2022-09-11 03:00:37 +0100

    Move `entrypoint` injection to the last layer

--- server/archiver/entrypoint.sh
+++ server/archiver/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-/usr/local/bin/docker-entrypoint.sh
-
 ./main --logtostderr \
     --host=postgres-postgresql-ha-pgpool --database-name=keyring \
     --json-creds-path="$CLOUD_CREDS_PATH" --bucket-name="$BUCKET_NAME"
