3ed926f84a6b310982ad6dc8ddd941cefa19e6ab
Pavel Dubov <dubov94@gmail.com>
2022-09-11 01:25:25 +0100

    Invoke docker-entrypoint.sh from archiver/entrypoint.sh

--- server/archiver/entrypoint.sh
+++ server/archiver/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+/usr/local/bin/docker-entrypoint.sh
+
 ./main --logtostderr \
     --host=postgres-postgresql-ha-pgpool --database-name=keyring \
     --json-creds-path="$CLOUD_CREDS_PATH" --bucket-name="$BUCKET_NAME"
