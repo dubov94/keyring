d15a996e565129534c94648cdcb05040d15920ea
Pavel Dubov <dubov94@gmail.com>
2022-12-24 05:53:27 +0000

    Set `STABLE_MRGN_VERSION` to c268436

--- bazel/workspace/print_vars.sh
+++ bazel/workspace/print_vars.sh
@@ -3,4 +3,4 @@
 # https://git-scm.com/docs/git-describe#_description
 echo STABLE_GIT_REVISION $(git describe --always)
 # Earliest acceptable version.
-echo STABLE_MRGN_REVISION 'v0.0.0-1026-g1760e08'
+echo STABLE_MRGN_REVISION 'v0.0.0-1029-gc268436'
