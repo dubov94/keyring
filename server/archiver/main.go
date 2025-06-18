ba7378f176ad73573b8550579bdcd45deedba718
Pavel Dubov <dubov94@gmail.com>
2025-06-18 03:20:10 +0100

    Clean up residual `archiver` logging

--- server/archiver/main.go
+++ server/archiver/main.go
@@ -49,7 +49,7 @@ func writeArchive(ctx context.Context) (fileName string, err error) {
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
 	if err != nil {
-		return "", fmt.Errorf("unable to dump database '%s': %w", *databaseName, err)
+		return "", fmt.Errorf("unable to dump database: %w", err)
 	}
 
 	return fileName, nil
