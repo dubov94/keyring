74435602b6d24b5e8feef53ebccfed6e9d8805c2
Pavel Dubov <dubov94@gmail.com>
2026-02-15 15:51:52 +0000

    Move CA installation into `entrypoint.sh` files

--- server/archiver/entrypoint.sh
+++ server/archiver/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+apt update
+apt install --assume-yes --no-install-recommends ca-certificates
+
 ./main --logtostderr --pg-dbname-path="$PG_DBNAME_PATH" \
     --json-creds-path="$CLOUD_CREDS_PATH" --bucket-name="$BUCKET_NAME"
--- server/restorer/entrypoint.sh
+++ server/restorer/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+apt update
+apt install --assume-yes --no-install-recommends ca-certificates
+
 ./main --logtostderr --pg-dbname-path="$PG_DBNAME_PATH" \
     --json-creds-path="$CLOUD_CREDS_PATH" --bucket-name="$BUCKET_NAME" \
     --object-name="$OBJECT_NAME"
