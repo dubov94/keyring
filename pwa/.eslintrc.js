2e947c12daa9c395ff48316511314802d34f6ea6
Pavel Dubov <dubov94@gmail.com>
2026-02-24 15:39:53 +0000

    Disable `no-console` in ESLint

--- pwa/.eslintrc.js
+++ pwa/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-use-before-define': 'off',
     'no-useless-constructor': 'off',
